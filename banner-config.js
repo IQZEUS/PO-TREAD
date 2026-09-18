59bad14281a4e24540080fbc3ac18afcd687e22a
IQZEUS <amirhosseindelfan000@gmail.com>
2026-09-18 21:30:33 +0330

    Update banner-config.js

--- banner-config.js
+++ banner-config.js
@@ -23,7 +23,7 @@ window.PT_BANNER_CONFIG = {
       priority: 10,
       type: 'info',
       icon: '🎉',
-      title: 'نسخه جدیککککککد منتشر شد!',
+      title: 'نسخه جدیکککک00ککد منتشر شد!',
       title_en: 'New version released!',
       message: 'قابلیت‌های جدید: تحلیل احساسات، تقویم پیشرفته و مودال زیبا',
       message_en: 'New features: emotion analysis, advanced calendar and beautiful modal',
